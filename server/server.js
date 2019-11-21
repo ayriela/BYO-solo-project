362b44ebbc9f6552219b3f9e086a4e7d2e17b140
Ayriel Ash <aya37278@bethel.edu>
2019-11-21 09:34:56 -0600

    create event basic form

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const restrictionRouter = require('./routes/restriction.router');
 const profileRouter = require('./routes/profile.router');
+const eventRouter = require('./routes/event.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -28,6 +29,7 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/restriction', restrictionRouter);
 app.use('/profile', profileRouter);
+app.use('/event', eventRouter);
 
 // Serve static files
 app.use(express.static('build'));
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -3,6 +3,7 @@ import loginSaga from './loginSaga';
 import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import restrictionSaga from './restrictionSaga';
+import eventSaga from './eventSaga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -17,5 +18,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     restrictionSaga(),
+    eventSaga(),
   ]);
 }
